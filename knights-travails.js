b74c140f8dccce614f7dc4e4f3e83f578bbcf181
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-01-02 10:29:00 +0700

    Add chessboard data structure

---
+++ knights-travails.js
@@ -0,0 +1,32 @@
+// Generate a chess board
+const BOARD = [];
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    const cell = {
+      coor: [j, i],
+      top: null,
+      right: null,
+      bottom: null,
+      left: null,
+    };
+    BOARD.push(cell);
+  }
+}
+
+// Link each cell to another to form a graph
+for (let i = 0; i < BOARD.length; i++) {
+  const x = BOARD[i].coor[0];
+  const y = BOARD[i].coor[1];
+  if (x !== 0) {
+    BOARD[i].left = i - 1;
+  }
+  if (x !== 7) {
+    BOARD[i].right = i + 1;
+  }
+  if (y !== 0) {
+    BOARD[i].bottom = i - 8;
+  }
+  if (y !== 7) {
+    BOARD[i].top = i + 8;
+  }
+}
